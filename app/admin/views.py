2cda9c608ba73764221a0fa9535781b4fa5895a0
yangcongtou <maxzhangcong@163.com>
2017-12-12 17:40:11 +0800

    后台页面搭建12121740

--- app/admin/views.py
+++ app/admin/views.py
@@ -7,9 +7,49 @@
     *************模块文档注释**************
 """
 from app.admin import admin
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 @admin.route('/')
 def index():
-    return '<h1>123</h1>'
+    return render_template('admin/index.html')
+
+@admin.route('/login/')
+def login():
+    return render_template('admin/login.html')
+
+@admin.route('/logout/')
+def logout():
+    return redirect(url_for('admin.login'))
+
+@admin.route('/pwd/')
+def pwd():
+    return render_template('admin/pwd.html')
+
+@admin.route('/tag/add/')
+def tag_add():
+    return render_template('admin/tag_add.html')
+
+@admin.route('/tag/list/')
+def tag_list():
+    return render_template('admin/tag_list.html')
+
+# @admin.route('/')
+# def index():
+#     return render_template('admin/index.html')
+#
+# @admin.route('/')
+# def index():
+#     return render_template('admin/index.html')
+#
+# @admin.route('/')
+# def index():
+#     return render_template('admin/index.html')
+#
+# @admin.route('/')
+# def index():
+#     return render_template('admin/index.html')
+#
+# @admin.route('/')
+# def index():
+#     return render_template('admin/index.html')
