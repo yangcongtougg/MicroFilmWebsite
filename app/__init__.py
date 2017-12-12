6de69de363c9d30bdf06f229f5f7b1398dccc5fe
yangcongtou <maxzhangcong@163.com>
2017-12-12 13:17:04 +0800

    前端页面搭建完毕12121317

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@
 """
     *************模块文档注释**************
 """
-from flask import Flask
+from flask import Flask, render_template
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
 
@@ -16,3 +16,7 @@ app.debug = True
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
+# 配置404页面
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('home/404.html'), 404
